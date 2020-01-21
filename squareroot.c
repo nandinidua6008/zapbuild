01fe899d6eae4a0009ace44652c0fea258efd5b6
nandinidua6008 <nandinidua6008@gmail.com>
2020-01-21 17:51:39 +0530

    square root of a number

---
+++ squareroot.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num,root;
+
+    /* Input a number from user */
+    printf("Enter any number to find square root: ");
+    scanf("%d", &num);
+
+    /* Calculate square root of num */
+    root = num*num;
+
+    /* Print the resultant value */
+    printf("Square root of %d is %d", num, root);
+
+    return 0;
+}
---
+++ sum of digits
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=12345,sum=0,rem;
+    while(n>0){
+        rem=n%10;
+        sum=sum+rem;
+        n=n/10;
+    }
+    printf("%d",sum);
+
+    return 0;
+}
+
