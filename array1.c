4724a16498ca6a7c7733145ef9c473277b110e8c
nandinidua6008 <nandinidua6008@gmail.com>
2020-01-22 12:24:38 +0530

    traversing of array

---
+++ array1.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a[10]={94,48,44,21,32,444,21,123,13,344},i;
+    printf("elements of std::array< is \n");
+    for(i=0;i<10;i++){
+        printf("%d ",a[i]);
+    }
+
+    return 0;
+}
+
