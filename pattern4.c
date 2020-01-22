235cc5f200bb9aed6bdc8f27c90122ef6a827c08
nandinidua6008 <nandinidua6008@gmail.com>
2020-01-22 11:37:54 +0530

    pattern4

---
+++ pattern4.c
@@ -0,0 +1,11 @@
+    #include<stdio.h>  
+    void main(){
+        int i,j,sum;
+        for(i=0;i<=4;i++){
+            for(j=1;j<=3;j++){
+                sum=i*j;
+                printf("%d ",sum);
+            }
+            printf("\n");
+        }
+    }
