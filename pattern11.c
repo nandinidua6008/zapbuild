62ef90cd5423ae438cf2c68ff722d83ed997a6a5
nandinidua6008 <nandinidua6008@gmail.com>
2020-01-23 13:25:04 +0530

    star pattern

---
+++ pattern11.c
@@ -0,0 +1,23 @@
+    #include<stdio.h>  
+    void main(){
+        int i,j,k,n,t;
+        printf("enter limit\n");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++){
+             k=1,t=1;
+            for(j=1;j<=(n*2)-1;j++){
+               
+                if((j>=(n+1)-i)&&(j<=(n-1)+i)&&t)
+               {
+                   printf("%d",k);
+                   j<n+i?k++:k--;
+                   t=0;
+               }
+                else{
+                printf(" ");
+                t=1;
+                }
+            }
+            printf("\n");
+        }
+    }
