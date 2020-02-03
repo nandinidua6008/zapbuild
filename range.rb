90493601364bebca8a6647b61edd15470de71d50
nandinidua6008 <nandinidua6008@gmail.com>
2020-02-03 16:11:23 +0530

    range program

---
+++ range.rb
@@ -0,0 +1,11 @@
+
+# to_a is used to convert it  
+# into array 
+range_op = (7 .. 10).to_a 
+   
+
+puts "#{range_op}"
+
+range_op1 = (7 ... 10).to_a 
+
+puts "#{range_op1}"
---
+++ range1.rb
@@ -0,0 +1,3 @@
+range=(7..10).to_a
+puts range
+puts "or we can write it as #{range}"
