f3b7a4d785158d73d91489a6aa2fc683e22e1452
nandinidua6008 <nandinidua6008@gmail.com>
2020-02-04 14:35:52 +0530

    array programs

---
+++ array3.rb
@@ -0,0 +1,8 @@
+numbers=[1,3,5,7,8] #collection of 5 numbers of Integer type
+puts numbers
+
+names=["nandini","naina","harpreet","isha"] #collection of 5 string
+puts names
+
+values=[1.23,23.23,2.33,93.22] #collectionof 4 float numbers of floating type
+puts values
---
+++ array4.rb
@@ -0,0 +1,6 @@
+numbers=[1,3,5,7,8] #collection of 5 numbers of Integer type
+puts numbers
+
+sum=numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]
+print("\n #{sum}")
+
