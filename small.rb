01ed562a7ea75e775ada17084d21433ab616b1af
nandinidua6008 <nandinidua6008@gmail.com>
2020-02-11 18:01:50 +0530

    sum of smallest 3 numbers in array

---
+++ small.rb
@@ -0,0 +1,26 @@
+a=[2,3,23,13,71,43,13,63]
+size=a.size
+i=0
+first=second=third=100
+puts "the array is "
+while i<size
+    print a[i]," "
+    i=i+1
+end
+puts "\nsun of smallest numbers are "
+i=0
+while i<size
+    if a[i]<first
+        third=second
+        second=first
+        first=a[i]
+    elsif a[i]<second
+        third=second
+        second=a[i]
+    elsif a[i]<third
+        third=a[i]
+    end
+    i=i+1
+end
+sum=first+second+third
+print first," + ",second," + ",third," = ",sum
